40bc4cba06d783b7cbe1becae9763da8c8d260f1
Arya Patel <arya@MacBook-Pro-84.local>
2024-02-23 14:26:28 -0600

    finished css

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,44 @@
-import { puppyList } from "./data.js";
-import "./path-to-css.css";
 import { useState } from "react";
+import { puppyList } from "./data.js";
 import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-  console.log("puupyList:", puppyList);
+  console.log("puppyList: ", puppyList);
+  function handleClick(clickedPupId) {
+    const clickedPuppy = puppies.find((puppy) => puppy.id === clickedPupId);
+    if (clickedPuppy) setFeatPupId(clickedPupId);
 
-  const featuredPup = puppies.find((pup) => pup.id === featPupId);
-  console.log("featuredPup:", featuredPup);
-
+    console.log(clickedPuppy);
+    const tricksString = clickedPuppy.tricks
+      .map((trick) => ` ${trick.title}`)
+      .join(", ");
+    alert(`
+      Puppy ID: ${clickedPuppy.id} \n
+      Name: ${clickedPuppy.name} \n
+      Age: ${clickedPuppy.age} \n
+      Email: ${clickedPuppy.email} \n
+      Tricks: ${tricksString}`);
+  }
   return (
     <div className="App">
-      {puppies.map((puppy) => {
-        return (
-          <p
-            onClick={() => {
-              setFeatPupId(puppy.id);
-            }}
-            key={puppy.id}
-          >
-            {puppy.name}
-          </p>
-        );
-      })}
-      {featPupId && (
-        <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Age: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
+      <div className="header">
+        <h1>List of Puppies</h1>
+      </div>
+      <div className="puppyApp">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              className="dog-name"
+              onClick={() => handleClick(puppy.id)}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 }
